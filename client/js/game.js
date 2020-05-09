b9db0b449c7ae85a47789fc9cfea5c9a534c0da3
Brian Price <brianprice88@gmail.com>
2020-05-08 22:02:47 -0400

    "update URL"

--- client/js/game.js
+++ client/js/game.js
@@ -6,6 +6,7 @@ var level;
 var clickListener = function (event) { board.triggerCell(event) } // event handler callback is added at beginning of game and removed at end, thus placed in global scope
 
 const URI = 'https://minesweeper-brian-price.herokuapp.com/highScores'
+// const URI = 'http://localhost:3000/highScores'
 
 var loadHighScores = function () {
     var scores = []
